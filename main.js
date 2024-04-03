087b5b799c962145a472d4e39f48da3a32de271f
nishanazar <zuhaibsoomro.sst@hotmail.com>
2024-04-03 14:43:36 +0500

    I creat ATM machine

--- main.js
+++ main.js
@@ -1,52 +1,55 @@
 import inquirer from "inquirer";
 let myBalance = 10000;
 let myPin = 1234;
-let pinAnswer = await inquirer.prompt([{
+let pinAnswer = await inquirer.prompt([
+    {
         name: "pin",
         message: "enter your pin",
-        type: "number"
-    }]);
-if (pinAnswer.pin === 1234) {
+        type: "number",
+    },
+]);
+if (pinAnswer.pin === myPin) {
     console.log("Correct pin code!!");
-    let operationAns = await inquirer.prompt([{
-            name: "operation",
-            message: "please select option",
+}
+let operationAns = await inquirer.prompt([
+    {
+        name: "operation",
+        message: "please select option",
+        type: "list",
+        choices: ["withdraw", "check balance", "fast cash"],
+    },
+]);
+if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+        {
+            name: "amount",
+            message: "enter your amount: ",
+            type: "number",
+        },
+    ]);
+    if (amountAns.amount <= myBalance) {
+        myBalance -= amountAns.amount;
+        console.log(`your remaining balance is: ${myBalance}`);
+    }
+    else {
+        console.log("Insufficient Balance!");
+    }
+}
+else if (operationAns.operation === "check balance") {
+    console.log(`your balance is: ${myBalance}`);
+}
+else if (operationAns.operation === "fast cash") {
+    let fastCashAns = await inquirer.prompt([
+        {
+            name: "fastCash",
+            message: "How much amounts you want to fast cash:",
             type: "list",
-            choices: ["withdraw", "check balance", "funds transfer"]
-        }]);
-    if (operationAns.operation === "withdraw") {
-        let amountAns = await inquirer.prompt([{
-                name: "amount",
-                message: "enter your amount",
-                type: "number"
-            }]);
-        if (amountAns.amount < myBalance) {
-            myBalance -= amountAns.amount;
-            console.log(`your remaining balance is: ${myBalance}`);
-        }
-        else {
-            console.log("Insufficient balance");
-        }
-    }
-    else if (operationAns.operation === "check balance") {
-        console.log(`your balance is: ${myBalance}`);
-    }
-    else if (operationAns.operation === "funds transfer") {
-        let transfer = await inquirer.prompt([{
-                name: "funds",
-                message: "money transfer",
-                type: "list",
-                choices: ["to any MCB account", "to any other bank account"]
-            }]);
-        if (transfer.funds == "to any MCB account") {
-            console.log("you sucessfuly login");
-        }
-        if (transfer.funds == "to any other bank account") {
-            console.log("please input account passward");
-        }
-    }
+            choices: [1000, 2000, 5000, 10000],
+        },
+    ]);
+    myBalance -= fastCashAns.fastCash;
+    console.log(`your remaining amount is: ${myBalance}`);
 }
 else {
     console.log("Incorrect pin number");
 }
-// console.log(myBalance);
